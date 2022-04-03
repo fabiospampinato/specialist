47753e97d25bda573f7781ca1eb161ade344d965
Fabio Spampinato <spampinabio@gmail.com>
2022-04-03 22:20:11 +0100

    Replaced "colorette" with "tiny-colors"

--- src/color.ts
+++ src/color.ts
@@ -1,6 +1,8 @@
 
+/* IMPORT */
+
+import color from 'tiny-colors';
+
 /* EXPORT */
 
-export {black, red, green, yellow, blue, magenta, cyan, white, gray} from 'colorette';
-export {bgBlack, bgRed, bgGreen, bgYellow, bgBlue, bgMagenta, bgCyan, bgWhite} from 'colorette';
-export {dim, bold, hidden, italic, underline, strikethrough, reset} from 'colorette';
+export default color;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 /* IMPORT */
 
 import updater from 'tiny-updater';
-import * as color from './color';
+import color from './color';
 import parseArgv from './parse_argv';
 import program from './program';
 
