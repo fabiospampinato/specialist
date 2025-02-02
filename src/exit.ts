8ae90fad6a148d07bb51a2a58d247122048de247
Fabio Spampinato <spampinabio@gmail.com>
2025-02-02 17:32:03 +0000

    exit: writing to either stdout or stderr automatically, depending on the exit code

--- src/exit.ts
+++ src/exit.ts
@@ -7,16 +7,19 @@ import color from 'tiny-colors';
 /* MAIN */
 
 // Little function modeled to have the same visual output as tiny-bin's "fail" function
+// It targets either stdout or stderr automatically, based on the exit code
 
 const exit = ( message: string, code: number = 1 ): never => {
 
+  const log = code === 0 ? console.log : console.error;
+
   if ( code === 0 ) {
 
-    console.log ( message );
+    log ( message );
 
   } else {
 
-    console.log ( `\n  ${color.red ( message )}\n` );
+    log ( `\n  ${color.red ( message )}\n` );
 
   }
 
